5082d702d4b696d4b057880df59b8f3f755c43f6
Pratik Agarwal <pratikkumar.agarwal123@gmail.com>
2023-06-12 20:10:47 +0530

    changes address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xA4a7d5Fb6906689CC33e3c40241EA2a8747eF2E1";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-three-gray.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
